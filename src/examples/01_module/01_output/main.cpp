e61a072a52af5a597ad04b5cfc5a11771c5e9f70
zdfehr1234 <zachary.fehr@g.austincc.edu>
2020-01-23 19:03:42 -0600

    Show include differences

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -1,9 +1,12 @@
-#include "output.h"
+#include "output.h"// Code I wrote
+#include<iostream>// Using standard library 
+
+using std::cout;
 
 
 int main() 
 {
-	
+	cout<<"Hello World";
 
 	return 0;
 }
