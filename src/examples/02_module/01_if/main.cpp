774fb21bf592ba094fb7f553a9f989a3501ff9e9
zdfehr1234 <zachary.fehr@g.austincc.edu>
2020-02-04 18:40:09 -0600

    Bool expressions, cin and if statements

--- src/examples/02_module/01_if/main.cpp
+++ src/examples/02_module/01_if/main.cpp
@@ -1,23 +1,33 @@
 #include<iostream>
 #include "if.h"//to use is_even function here
 
-using std::cout;
-using std::cin;
+using std::cout; // Outputs to screen 
+using std::cin; // Used to capture keyboard
 
 int main() 
 {
 	//create a bool variable named even
+	bool even;
 	//create an int variable named value
-
+	int value;
 	//prompt user for a keyboard number
+	cout << "Type a number: ";
 	//assign the number to the value variable
-
+	cin >> value;
 	//call the is_even function with value as its argument
 	//and assign its return value to the even variable
+	even == is_even(value);
+
 	
 
 	//display even if number is even
-
-
+	if (even == true)
+	{
+		cout << value << "is even.";
+	}
+	else
+	{
+		cout << value << "is odd.";
+	}
 	return 0;
 }
