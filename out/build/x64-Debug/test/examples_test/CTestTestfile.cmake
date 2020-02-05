6e9eb1b8a88854a8177eb8d78246c1d13af1e003
Zachary Fehr <zachary.fehr@g.austincc.edu>
2020-02-04 18:06:41 -0800

    Unmuted CMakeLists.txt for 01 module

---
+++ out/build/x64-Debug/test/examples_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/test/examples_test
+# Build directory: C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/out/build/x64-Debug/test/examples_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("01_module_test")
+subdirs("02_module_test")
