d63ff1f8efc3d680fedcffd00d868c3612378bbc
zdfehr1234 <zachary.fehr@g.austincc.edu>
2020-02-11 18:44:06 -0600

    Example 3 for loops

--- src/examples/03_module/02_for/for.cpp
+++ src/examples/03_module/02_for/for.cpp
@@ -1,5 +1,7 @@
-//#include "sample_for.h"
+
 #include<iostream>
+#include "for.h"
+
 
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
@@ -15,3 +17,10 @@ RESULT:
 
 */
 
+void display_numbers(int num)
+{
+	for (int i = 0; i < num; ++i)
+	{
+		std::cout << i + 1 << "\n";
+	}
+}
--- src/examples/03_module/02_for/main.cpp
+++ src/examples/03_module/02_for/main.cpp
@@ -1,7 +1,15 @@
 #include"for.h"
+#include<iostream>
+
+using std::cout; using std::cin; 
 
 int main() 
 {
-	
+	int num;
+	cout << "Enter a number";
+	cin >> num; 
+
+	display_numbers(num);
+
 	return 0;
 }
