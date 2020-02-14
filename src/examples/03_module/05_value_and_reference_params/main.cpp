1bea8121e394b780d560b3ef41850fa67f662591
zdfehr1234 <zachary.fehr@g.austincc.edu>
2020-02-13 18:31:09 -0600

    ref, value, const ref paramaters examples

--- src/examples/03_module/05_value_and_reference_params/main.cpp
+++ src/examples/03_module/05_value_and_reference_params/main.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 
+using std::cout;
+
 int main() 
 {
+	int num = 5;
+	cout << "Value of: " << num << " Address is: " << &num << "\n";
+
+	int &num_ref = num;
+	num_ref = 10;
+	cout << "Value of: " << num << " Address is: " << &num<<"\n";
+
 
 	return 0;
 }
