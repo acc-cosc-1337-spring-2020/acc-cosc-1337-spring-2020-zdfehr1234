e354001683af4ccb59105b719d500d6ed6e2d4b9
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-13 21:58:16 -0600

    remove

--- out/build/x64-Debug/test/homework_test/02_decisions_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/test/homework_test/02_decisions_test
-# Build directory: C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/out/build/x64-Debug/test/homework_test/02_decisions_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hw_02_tests "C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/out/build/x64-Debug/test/homework_test/02_decisions_test/hw_02_tests.exe" "-s")
-set_tests_properties(hw_02_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/test/homework_test/02_decisions_test/CMakeLists.txt;3;add_test;C:/Users/zferh/source/repos/acc-cosc-1337-spring-2020-zdfehr1234/test/homework_test/02_decisions_test/CMakeLists.txt;0;")
