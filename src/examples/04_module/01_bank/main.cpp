66f16ca2a9f8191dab95c441bb2bc0af9ab1a98f
zdfehr1234 <zachary.fehr@g.austincc.edu>
2020-02-25 19:07:30 -0600

    Object oriented programing and classes

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,21 @@
+#include "bank_account.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 int main()
 {
+	BankAccount account(500); 
+
+	auto balance = account.get_balance();
+	cout << "Balance is : " << balance; 
+
+	auto amount{ 0 };
+	cout << "Enter deposit amount: ";
+	cin >> amount;
+
+	account.deposit(amount);
+	cout << "Balance is: ";
 
 	return 0;
 }
--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -1,7 +1,41 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test BankAccount constructor")
+{
+	BankAccount account(500);
+
+	REQUIRE(account.get_balance() == 500);
+}
+
+TEST_CASE("Test bank account deposit")
+{
+	BankAccount account(500);
+	REQUIRE(account.get_balance() == 500);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 550);
+
+	account.deposit(-50);
+	REQUIRE(account.get_balance() == 550);
+}
+
+TEST_CASE("Test BankAccount withdraw")
+{
+	BankAccount account(500);
+	REQUIRE(account.get_balance() == 500);
+
+	account.withdraw(50);
+	REQUIRE(account.get_balance() == 450);
+
+	account.withdraw(-50);
+	REQUIRE(account.get_balance() == 450);
+
+	account.withdraw(500);
+	REQUIRE(account.get_balance() == 450);
+}
