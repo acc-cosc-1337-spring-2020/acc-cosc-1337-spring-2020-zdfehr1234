e0c7100bc973a3c2a0b5eb90511424efee810070
Zachary Fehr <zachary.fehr@g.austincc.edu>
2020-04-16 18:56:10 -0500

    Dynamic memory example

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+
+using std::cout;
+
+
 int main() 
 {
-	
+	int* ptr_num = new int(5);
+
+	cout << "Address of ptr_num: " << &ptr_num << "\n";//stack address
+	cout << "Address ptr_num points to: " << ptr_num << "\n"; //Heap address
+	cout << "Value at address ptr_num points to: " << *ptr_num << "\n"; //Actual value
+
+	delete ptr_num;
+	ptr_num = nullptr;
+
+
 	return 0;
 }
